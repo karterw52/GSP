51c1565ca73b4510818b4d38e55b74905283e7bb
jacksonm85 <jacksonm85@kprschools.ca>
2023-12-15 10:41:38 -0500

    changed base game code

--- main.py
+++ main.py
@@ -1,191 +1,331 @@
-# Simple Snake Game in Python 3 for Beginners
-# By @TokyoEdTech
+# ************************************
 
-import turtle
-import time
+# Python Snake
+
+# ************************************
+
+from tkinter import *
+
 import random
 
-delay = 0.1
 
-# Score
+
+GAME_WIDTH = 700
+
+GAME_HEIGHT = 700
+
+SPEED = 50
+
+SPACE_SIZE = 50
+
+BODY_PARTS = 3
+
+SNAKE_COLOR = "#00FF00"
+
+FOOD_COLOR = "#FF0000"
+
+BACKGROUND_COLOR = "#000000"
+
+
+
+
+
+class Snake:
+
+
+
+    def __init__(self):
+
+        self.body_size = BODY_PARTS
+
+        self.coordinates = []
+
+        self.squares = []
+
+
+
+        for i in range(0, BODY_PARTS):
+
+            self.coordinates.append([0, 0])
+
+
+
+        for x, y in self.coordinates:
+
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+
+            self.squares.append(square)
+
+
+
+
+
+class Food:
+
+
+
+    def __init__(self):
+
+
+
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+
+
+
+        self.coordinates = [x, y]
+
+
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+
+
+
+
+
+def next_turn(snake, food):
+
+
+
+    x, y = snake.coordinates[0]
+
+
+
+    if direction == "up":
+
+        y -= SPACE_SIZE
+
+    elif direction == "down":
+
+        y += SPACE_SIZE
+
+    elif direction == "left":
+
+        x -= SPACE_SIZE
+
+    elif direction == "right":
+
+        x += SPACE_SIZE
+
+
+
+    snake.coordinates.insert(0, (x, y))
+
+
+
+    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+
+
+
+    snake.squares.insert(0, square)
+
+
+
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+
+
+
+        global score
+
+
+
+        score += 1
+
+
+
+        label.config(text="Score:{}".format(score))
+
+
+
+        canvas.delete("food")
+
+
+
+        food = Food()
+
+
+
+    else:
+
+
+
+        del snake.coordinates[-1]
+
+
+
+        canvas.delete(snake.squares[-1])
+
+
+
+        del snake.squares[-1]
+
+
+
+    if check_collisions(snake):
+
+        game_over()
+
+
+
+    else:
+
+        window.after(SPEED, next_turn, snake, food)
+
+
+
+
+
+def change_direction(new_direction):
+
+
+
+    global direction
+
+
+
+    if new_direction == 'left':
+
+        if direction != 'right':
+
+            direction = new_direction
+
+    elif new_direction == 'right':
+
+        if direction != 'left':
+
+            direction = new_direction
+
+    elif new_direction == 'up':
+
+        if direction != 'down':
+
+            direction = new_direction
+
+    elif new_direction == 'down':
+
+        if direction != 'up':
+
+            direction = new_direction
+
+
+
+
+
+def check_collisions(snake):
+
+
+
+    x, y = snake.coordinates[0]
+
+
+
+    if x < 0 or x >= GAME_WIDTH:
+
+        return True
+
+    elif y < 0 or y >= GAME_HEIGHT:
+
+        return True
+
+
+
+    for body_part in snake.coordinates[1:]:
+
+        if x == body_part[0] and y == body_part[1]:
+
+            return True
+
+
+
+    return False
+
+
+
+
+
+def game_over():
+
+
+
+    canvas.delete(ALL)
+
+    canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2,
+
+                       font=('consolas',70), text="GAME OVER", fill="red", tag="gameover")
+
+
+
+
+
+window = Tk()
+
+window.title("Snake game")
+
+window.resizable(False, False)
+
+
+
 score = 0
-high_score = 0
 
-# Set up the screen
-wn = turtle.Screen()
-wn.title("Snake Game by @TokyoEdTech")
-wn.bgcolor("green")
-wn.setup(width=600, height=600)
-wn.tracer(0)  # Turns off the screen updates
-
-# Snake head
-head = turtle.Turtle()
-head.speed(0)
-head.shape("square")
-head.color("black")
-head.penup()
-head.goto(0, 0)
-head.direction = "stop"
-
-# Snake food
-food = turtle.Turtle()
-food.speed(0)
-food.shape("circle")
-food.color("red")
-food.penup()
-food.goto(0, 100)
-
-segments = []
-
-# Pen
-pen = turtle.Turtle()
-pen.speed(0)
-pen.shape("square")
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 260)
-pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+direction = 'down'
 
 
-# Functions
-def go_up():
-    if head.direction != "down":
-        head.direction = "up"
+
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+
+label.pack()
 
 
-def go_down():
-    if head.direction != "up":
-        head.direction = "down"
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+
+canvas.pack()
 
 
-def go_left():
-    if head.direction != "right":
-        head.direction = "left"
+
+window.update()
 
 
-def go_right():
-    if head.direction != "left":
-        head.direction = "right"
+
+window_width = window.winfo_width()
+
+window_height = window.winfo_height()
+
+screen_width = window.winfo_screenwidth()
+
+screen_height = window.winfo_screenheight()
 
 
-def move():
-    if head.direction == "up":
-        y = head.ycor()
-        head.sety(y + 20)
 
-    if head.direction == "down":
-        y = head.ycor()
-        head.sety(y - 20)
+x = int((screen_width/2) - (window_width/2))
 
-    if head.direction == "left":
-        x = head.xcor()
-        head.setx(x - 20)
-
-    if head.direction == "right":
-        x = head.xcor()
-        head.setx(x + 20)
+y = int((screen_height/2) - (window_height/2))
 
 
-# Keyboard bindings
-wn.listen()
-wn.onkeypress(go_up, "w")
-wn.onkeypress(go_down, "s")
-wn.onkeypress(go_left, "a")
-wn.onkeypress(go_right, "d")
 
-# Main game loop
-while True:
-    wn.update()
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-    # Check for a collision with the border
-    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
-        time.sleep(1)
-        head.goto(0, 0)
-        head.direction = "stop"
 
-        # Hide the segments
-        for segment in segments:
-            segment.goto(1000, 1000)
 
-        # Clear the segments list
-        segments.clear()
+window.bind('<Left>', lambda event: change_direction('left'))
 
-        # Reset the score
-        score = 0
+window.bind('<Right>', lambda event: change_direction('right'))
 
-        # Reset the delay
-        delay = 0.1
+window.bind('<Up>', lambda event: change_direction('up'))
 
-        pen.clear()
-        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+window.bind('<Down>', lambda event: change_direction('down'))
 
-        # Check for a collision with the food
-    if head.distance(food) < 20:
-        # Move the food to a random spot
-        x = random.randint(-290, 290)
-        y = random.randint(-290, 290)
-        food.goto(x, y)
 
-        # Add a segment
-        new_segment = turtle.Turtle()
-        new_segment.speed(0)
-        new_segment.shape("square")
-        new_segment.color("grey")
-        new_segment.penup()
-        segments.append(new_segment)
 
-        # Shorten the delay
-        delay -= 0.001
+snake = Snake()
 
-        # Increase the score
-        score += 10
+food = Food()
 
-        if score > high_score:
-            high_score = score
 
-        pen.clear()
-        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
 
-        # Move the end segments first in reverse order
-    for index in range(len(segments) - 1, 0, -1):
-        x = segments[index - 1].xcor()
-        y = segments[index - 1].ycor()
-        segments[index].goto(x, y)
+next_turn(snake, food)
 
-    # Move segment 0 to where the head is
-    if len(segments) > 0:
-        x = head.xcor()
-        y = head.ycor()
-        segments[0].goto(x, y)
 
-    move()
 
-    # Check for head collision with the body segments
-    for segment in segments:
-        if segment.distance(head) < 20:
-            time.sleep(1)
-            head.goto(0, 0)
-            head.direction = "stop"
-
-            # Hide the segments
-            for segment in segments:
-                segment.goto(1000, 1000)
-
-            # Clear the segments list
-            segments.clear()
-
-            # Reset the score
-            score = 0
-
-            # Reset the delay
-            delay = 0.1
-
-            # Update the score display
-            pen.clear()
-            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center",
-                      font=("Courier", 24, "normal"))
-
-    time.sleep(delay)
-
-wn.mainloop()
+window.mainloop()
